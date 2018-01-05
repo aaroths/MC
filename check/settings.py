17d2f7f1d3979cd5e158b3146a5b1d73b68181f9
Aaron Rothstein <aaroths@gmail.com>
2018-01-05 11:49:58 -0600

    change

--- check/settings.py
+++ check/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cp(0)x*%!toluddgxd!$!9!lr7a&op**jb%$8)tbgal*&#b1*$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [aaroths.pythonanywhere.com]
 
 
 # Application definition
